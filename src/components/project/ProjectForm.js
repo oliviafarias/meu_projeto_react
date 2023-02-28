ab38fa685b7279d20e1522f6cc69e9740996c63c
Olivia Farias <olivia_jull182@hotmail.com>
2023-02-28 16:42:55 -0300

    4th commit

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -2,8 +2,26 @@ import styles from './ProjectForm.module.css'
 import Input from '../form/Input'
 import Multiselect from '../form/Multiselect'
 import SubmitButton from '../form/SubmitButton'
+import { useEffect, useState } from 'react'
 
 function ProjectForm ({ btnText }) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input type="text" 
@@ -18,7 +36,10 @@ function ProjectForm ({ btnText }) {
                     placeholder="Insira o orcamento total do projeto...">
             </Input>
 
-            <Multiselect name="category_id" text="Selecione a categoria"></Multiselect>
+            <Multiselect name="category_id" 
+                        text="Selecione a categoria" 
+                        options={categories}>
+            </Multiselect>
 
             <SubmitButton text={btnText}/>
 
